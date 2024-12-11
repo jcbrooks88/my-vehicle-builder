ed8db092fa239f3fa50e8f9baec0b20ada0586d6
UNC Coding Bootcamp <UNCBootcamp@Jefferys-MacBook.local>
2024-12-11 12:31:16 -0500

    Truck and Motorbike ts updates

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -29,11 +29,12 @@ constructor(
   weight: number,
   topSpeed: number,
   wheels: Wheel[]
-    ) {
-  // Call the constructor of the parent class, Vehicle
+  ) {
+    
+// Call the constructor of the parent class, Vehicle
   super();
   
-  // Initialize properties of the Motorbike class
+// Initialize properties of the Motorbike class
   this.vin = vin;
   this.color = color;
   this.make = make;
@@ -41,30 +42,32 @@ constructor(
   this.year = year;
   this.weight = weight;
   this.topSpeed = topSpeed;
-  // Check if the wheels array has 2 elements
-  // If not, create 2 new Wheel objects
-  // Otherwise, use the provided wheels array
-  if (wheels.length !== 2) {
-    this.wheels = [new Wheel(), new Wheel(),];
-  } else {
-    this.wheels = wheels;
-      }
-    }
+  this.wheels = wheels;
+
+// Check if the wheels array has 2 elements
+// If not, create 2 new Wheel objects
+// Otherwise, use the provided wheels array
+if (wheels.length !== 2) {
+  this.wheels = [new Wheel(), new Wheel(),];
+} else {
+  this.wheels = wheels;
+  }
+}
     
-  // TODO: Implement the wheelie method
-  // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
-  wheelie (): void {
+// TODO: Implement the wheelie method
+// TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
+wheelie (): void {
   console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`)
   }
 
-  // TODO: Override the printDetails method from the Vehicle class
+// TODO: Override the printDetails method from the Vehicle class
   override printDetails(): void {
 
-  // TODO: The method should call the printDetails method of the parent class
+// TODO: The method should call the printDetails method of the parent class
   super.printDetails();
 
-  // TODO: The method should log the details of the Motorbike
-  // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
+// TODO: The method should log the details of the Motorbike
+// TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
   console.log(`VIN: ${this.vin}`);
   console.log(`Color: ${this.color}`);
   console.log(`Make: ${this.make}`);
@@ -72,6 +75,7 @@ constructor(
   console.log(`Year: ${this.year}`);
   console.log(`Weight: ${this.weight} lbs`);
   console.log(`Top Speed: ${this.topSpeed} mph`);
+  console.log(`Wheels: ${this.wheels.length}`);
  }
 }
 
